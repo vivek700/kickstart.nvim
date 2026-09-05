b1af6596d348d7527ee105869a375f2e9d03de37
orip <oriori1703@gmail.com>
2026-09-05 16:16:39 +0300

    Use tsc (typescript7's builtin lsp) instead of ts_ls and update the plugin example

--- init.lua
+++ init.lua
@@ -736,13 +736,13 @@ do
     -- clangd = {},
     -- gopls = {},
     -- pyright = {},
-    -- rust_analyzer = {},
+    -- tsc = {},
     --
-    -- Some languages (like typescript) have entire language plugins that can be useful:
-    --    https://github.com/pmizio/typescript-tools.nvim
+    -- Some languages (like rust) have entire language plugins that can be useful:
+    --    https://github.com/mrcjkb/rustaceanvim
     --
     -- But for many setups, the LSP (`ts_ls`) will work just fine
-    -- ts_ls = {},
+    -- rust_analyzer = {},
 
     stylua = {}, -- Used to format Lua code
 
